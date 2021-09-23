a6290f3ec51223ac29b284b84332241b484617e9
Irongade <adeoluayangade@yahoo.com>
2021-09-23 11:46:01 +0100

    optimize code for deployment

--- src/route.js
+++ src/route.js
@@ -1,16 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import CoachesList from './pages/coaches/CoachesList.vue'
-import CoachDetails from './pages/coaches/CoachDetails.vue'
-import CoachRegisteration from './pages/coaches/CoachRegisteration.vue'
+// import CoachDetails from './pages/coaches/CoachDetails.vue'
+// import CoachRegisteration from './pages/coaches/CoachRegisteration.vue'
 
 import ContactCoach from './pages/requests/ContactCoach.vue'
-import ReceivedRequests from './pages/requests/ReceivedRequests.vue'
+// import ReceivedRequests from './pages/requests/ReceivedRequests.vue'
 
-import UserAuth from './pages/auth/UserAuth.vue'
+// import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue'
 
 import store from './store/index'
+
+const CoachDetails = () => import('./pages/coaches/CoachDetails.vue')
+const CoachRegisteration = () => import('./pages/coaches/CoachRegisteration.vue')
+const ReceivedRequests = () => import('./pages/requests/ReceivedRequests.vue')
+const UserAuth = () => import('./pages/auth/UserAuth.vue')
 
 const router = createRouter({
     history: createWebHistory(),
