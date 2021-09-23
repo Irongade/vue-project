8e64722322c76defaef6af2468c73847a32d8dd1
Irongade <adeoluayangade@yahoo.com>
2021-09-23 12:04:34 +0100

    add deployment config and deploy build

--- src/route.js
+++ src/route.js
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import CoachesList from './pages/coaches/CoachesList.vue'
-// import CoachDetails from './pages/coaches/CoachDetails.vue'
-// import CoachRegisteration from './pages/coaches/CoachRegisteration.vue'
-
 import ContactCoach from './pages/requests/ContactCoach.vue'
-// import ReceivedRequests from './pages/requests/ReceivedRequests.vue'
-
-// import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue'
-
 import store from './store/index'
 
 const CoachDetails = () => import('./pages/coaches/CoachDetails.vue')
